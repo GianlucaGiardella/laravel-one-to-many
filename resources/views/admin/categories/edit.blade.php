c241350d034fb34b75bd58b89f94894282ec7974
GianluGiarde <gianlugiarde@gmail.com>
2023-07-10 16:14:08 +0200

    fix category values

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
-                value="{{ old('name') }}">
+                value="{{ old('name', $category->name) }}">
             @error('name')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -21,7 +21,7 @@
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea class="form-control @error('description') is-invalid @enderror" id="description" rows="3"
-                name="description">{{ old('description') }}</textarea>
+                name="description">{{ old('description', $category->description) }}</textarea>
             @error('description')
                 <div class="invalid-feedback">
                     {{ $message }}
