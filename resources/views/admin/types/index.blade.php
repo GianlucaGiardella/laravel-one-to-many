7c58e459f1e8543fe0fedaee4f511cfcfe1ebbcd
GianluGiarde <gianlugiarde@gmail.com>
2023-07-11 16:27:29 +0200

    fix type delete

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -19,30 +19,30 @@
                     <td>
                         <a class="btn btn-primary" href="{{ route('admin.types.show', ['type' => $type->id]) }}">View</a>
                         <a class="btn btn-warning" href="{{ route('admin.types.edit', ['type' => $type->id]) }}">Edit</a>
-                        <button class="btn btn-danger js-delete" data-bs-toggle="modal" data-bs-target="#deleteModal"
-                            data-id="{{ $type->id }}">Delete</button>
+                        {{-- <button class="btn btn-danger js-delete" data-bs-toggle="modal" data-bs-target="#deleteModal"
+                            data-id="{{ $type->id }}">Delete</button> --}}
                     </td>
                 </tr>
             @endforeach
 
-            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            {{-- <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure ?</h1>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
                             <form action="" data-template="{{ route('admin.types.destroy', ['type' => '*****']) }}"
-                                method="type" class="d-inline-block" id="confirm-delete">
+                                method="post" class="d-inline-block" id="confirm-delete">
                                 @csrf
                                 @method('delete')
-                                <button class="btn btn-danger">Delete</button>
+                                <button class="btn btn-danger">Yes</button>
                             </form>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
         </tbody>
     </table>
